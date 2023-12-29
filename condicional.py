2849e7c1972808ec8fc64aeaaa5f575cefade295
João Victor Leoni <joaovictorleoni2021@gmail.com>
2023-12-29 09:22:01 -0300

    Create condicional.py

---
+++ condicional.py
@@ -0,0 +1,14 @@
+x = 3
+y = 3
+
+if y > x:
+    print('y é maior do que x')
+    print('Codigo dentro do bloco condicional IF')
+
+elif y < x:
+    print('y é menor do que x')
+
+elif y == x:
+    print('y é igual a x')
+
+print('Codigo fora do bloco condicional')
